59e2d62d0be8ef661ad26e067d549279db0e37b4
Orla McCaffrey <omccaff1@Orlas-MacBook-Pro.local>
2019-02-16 16:42:39 -0500

    working lightbulb

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$("img.light.on").on("click", function (e) {
+		e.preventDefault();
+
+		$("body").addClass("light-off");
+	});
+
+	$("img.light.off").on("click", function (e) {
+		e.preventDefault();
+
+		$("body").removeClass("light-off");
+	});
+
+});
